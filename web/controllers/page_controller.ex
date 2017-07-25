3c980b59b7bfbb32df83c031d3e28784ba2c2b5d
jimmay5469 <jimmay5469@gmail.com>
2017-07-25 15:12:57 -0400

    Show Strava activities

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,16 @@ defmodule Stravelixm.PageController do
   use Stravelixm.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    activities = []
+    if strava = get_session(conn, :strava) do
+      HTTPoison.start
+      {:ok, response} = HTTPoison.get(
+        "https://www.strava.com/api/v3/athlete/activities",
+        [Authorization: "Bearer #{strava["access_token"]}"]
+      )
+      activities = Poison.decode!(response.body)
+    end
+    render conn, "index.html", activities: activities
   end
 
   def token_exchange(conn, %{"code" => code}) do
